ae3bd4a6f54cec0febf232616cfc6d6f7172b418
Héctor Cabrera <me@cabrerahector.com>
2025-07-10 12:49:01 -0400

    Adds base template

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSV Parser</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <main>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <label for="csv_file">Select your .csv file:</label>
+            <input type="file" id="csv_file" name="csv_file" accept=".csv" required />
+            <button>Upload</button>
+        </form>
+    </main>
+</body>
+</html>
